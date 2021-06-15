1dcf378f9a1c13d0f13ffd390be0b0c4b63bc86a
Maks <kolomietsmaks1305@gmail.com>
2021-06-15 20:27:57 +0300

    1_commit

---
+++ HTML & CSS/Practice/js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+// const age = prompt('Вам есть уже 18?', '');
+// console.log(age);
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Ivan";
+
+alert(`Привет, ${user}`);
+
+
